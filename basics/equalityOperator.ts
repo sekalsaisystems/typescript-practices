c0290e278141c838e57610d8e806c3ea8fdce256
sekalsaisystems <sekalsaisystems@gmail.com>
2026-01-21 12:57:31 +0530

    Add TypeScript basics examples

---
+++ basics/equalityOperator.ts
@@ -0,0 +1,6 @@
+let x:number=10;
+let y:number=10;
+
+console.log(x==y);// checks value only - this is called loose equality
+
+console.log(x===y);//checks value and type - this is called strict equality
